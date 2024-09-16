848ef85cb9c73536c1b9d9b55ce519f74dd94842
Soujatya Das <soujatya548@gmail.com>
2024-09-16 21:32:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,106 @@
 import streamlit as st
-import pandas as pd
-import math
-from pathlib import Path
+from langchain.document_loaders.sitemap import SitemapLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
+from langchain_groq import ChatGroq
+from langchain.embeddings import HuggingFaceEmbeddings
+import sentence_transformers
+from langchain_community.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain.document_loaders import WebBaseLoader
+import requests
+from bs4 import BeautifulSoup
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+#Steamlite title
+st.title("Website Intel and Comparer")
+
+#LLM
+api_key = "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"
+
+llm = ChatGroq(groq_api_key = api_key, model_name = 'llama-3.1-70b-versatile', temperature = 0.2, top_p = 0.2)
+
+#Embedding
+hf_embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+
+sitemap_urls = [https://www.bajajallianzlife.com/sitemap.xml, https://www.hdfclife.com/sitemap.xml, https://www.tataaia.com/sitemap.xml]
+
+filter_urls = ["saral", "pension"]
+
+all_urls = []
+filtered_urls = []
+loaded_docs = []
+
+for sitemap_url in sitemap_urls:
+  response = requests.get(sitemap_url)
+  sitemap_content = response.content
+
+  #Parse sitemap URL
+  soup = BeautifulSoup(sitemap_content, 'lxml')
+  urls = [loc.text for loc in soup.find_all('loc')]
+
+  #Filter URLs
+  selected_urls = [url for url in urls if any(filter in url for filter in filter_urls)]
+
+  #Append URLs to the main list
+  filtered_urls.extend(selected_urls)
+
+  for url in filtered_urls:
+    try:
+      print(f"Loading URL: {url}")
+      loader = WebBaseLoader(url)
+      docs = loader.load()
+      loaded_docs.extend(docs)
+      st.success("Successfully loaded content")
+    except Exception as e:
+      st.error("Error")
+
+st.write(f"Loaded urls: {len(filtered_urls)}")
+
+#Craft ChatPrompt Template
+prompt = ChatPromptTemplate.from_template(
+"""
+You are an HDFC Life Insurance specialist who needs to answer queries based on the information provided in the websites. Please follow all the websites, and answer as per the same.
+Do not answer anything out of the website information.
+Do not skip any information from the context. Answer appropriately as per the query asked.
+Now, being an excellent HDFC Life Insurance agent, you need to compare your policies against the other company's policies in the websites.
+Generate tabular data wherever required to classify the difference between different parameters of policies.
+I will tip you with a $1000 if the answer provided is helpful.
+<context>
+{context}
+</context>
+Question: {input}""")
+
+#Text Splitting
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1500,
+    chunk_overlap  = 100,
+    length_function = len,
 )
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+document_chunks = text_splitter.split_documents(docs)
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+#Vector database storage
+vector_db = FAISS.from_documents(document_chunks, hf_embedding)
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+#Streamlit user query
+query = st.text_input("Enter your question")
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+if query:
+    #Perform similarity search
+    docs = vector_db.similarity_search(query)
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+    #Stuff Document Chain Creation
+    document_chain = create_stuff_documents_chain(llm, prompt)
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+    #Retriever from Vector store
+    retriever = vector_db.as_retriever()
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+    #Create a retrieval chain
+    retrieval_chain = create_retrieval_chain(retriever,document_chain)
 
-    return gdp_df
+    response = retrieval_chain.invoke({"input": query})
 
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+    st.write(response['answer'])
