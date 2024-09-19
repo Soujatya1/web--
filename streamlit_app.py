e51c3fd6e4898a1efc5f19410e88b9323a20b22d
Soujatya Das <soujatya548@gmail.com>
2024-09-20 02:03:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -157,7 +157,7 @@ if st.button("Load and Process"):
         ivf_index.add(vectors_np)
 
         # Store in FAISS vector database
-        st.session_state['vector_db'] = FAISS(document_chunks, hf_embedding, index=quantizer)
+        st.session_state['vector_db'] = FAISS(document_chunks, hf_embedding)
 
         # Stuff Document Chain Creation
         document_chain = create_stuff_documents_chain(llm, prompt)
