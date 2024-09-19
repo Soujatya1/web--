dc4cffdeba671114976c755151e9f88f1780a075
Soujatya Das <soujatya548@gmail.com>
2024-09-19 20:45:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_chains import RetrievalQA
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_chains.combine_documents import create_stuff_documents_chain
-from langchain_chains import create_retrieval_chain
+from langchain.chains import create_retrieval_chain
 from langchain.document_loaders import WebBaseLoader
 from langchain.document_loaders import PyPDFLoader
 import requests
