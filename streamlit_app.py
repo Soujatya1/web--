bc98e369e72ec06a25fc82c0e5be1cc98fab4ecc
Soujatya Das <soujatya548@gmail.com>
2024-09-19 20:46:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ from langchain.chains import RetrievalQA
 from langchain_groq import ChatGroq
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_chains import RetrievalQA
+from langchain.chains import RetrievalQA
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain.document_loaders import WebBaseLoader
 from langchain.document_loaders import PyPDFLoader
