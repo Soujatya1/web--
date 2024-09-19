cef829c232dbee2d4a02b6db5f45c38987b5e9d8
Soujatya Das <soujatya548@gmail.com>
2024-09-20 01:08:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ if st.button("Load and Process"):
     # LLM and Embeddings Initialization
     if api_key:
         llm = ChatGroq(groq_api_key=api_key, model_name='llama-3.1-70b-versatile', temperature=0.2, top_p=0.2)
-        hf_embedding = SentenceTransformer(model_name="sentence-transformers/paraphrase-MiniLM-L6-v2")
+        hf_embedding = SentenceTransformer("sentence-transformers/paraphrase-MiniLM-L6-v2")
 
         # Craft ChatPrompt Template
         prompt = ChatPromptTemplate.from_template(
