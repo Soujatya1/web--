1fa4d8f4c9bed2cd8ef44e68f5ebd2006b182e8b
Soujatya Das <soujatya548@gmail.com>
2024-09-20 09:37:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,7 +123,7 @@ query = st.text_input("Enter your query:")
 if st.button("Get Answer") and query:
    if st.session_state['retrieval_chain']:
        response = st.session_state['retrieval_chain'].invoke({"input": query})
-      st.write("Response:")
-      st.write(response['answer'])
+       st.write("Response:")
+       st.write(response['answer'])
    else:
-      st.write("Please load and process documents first.")
+       st.write("Please load and process documents first.")
